9b67c6b1f48beba8ab77fdef283c24f257733448
AzEnTa <azentagaming@gmail.com>
2025-11-25 19:03:37 +0100

    a

--- src/systems/wave_manager.py
+++ src/systems/wave_manager.py
@@ -24,3 +24,21 @@ class WaveManager:
     def are_all_waves_cleared(self):
         # Vérifier si toutes les vagues sont terminées
         pass
+
+class WaweManager:
+    def __init__(self):
+        self.active_waves = []
+    
+    def enqueue(self, wave):
+        self.active_waves.append(wave)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.active_waves.pop(0)
+        return None
+
+    def is_empty(self):
+        return len(self.active_waves) == 0
+    
+    def size(self):
+        return len(self.active_waves)
