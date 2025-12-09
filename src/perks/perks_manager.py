2d46b011f18d14efbe5a111346c4bc749638a736
JujuCXI <juli1.renarddelaunay@gmail.com>
2025-12-09 13:33:25 +0100

    changes

--- src/perks/perks_manager.py
+++ src/perks/perks_manager.py
@@ -1,20 +1,27 @@
 import random
 from .perks import Perks
 
-class PerkManager:
-    def __init__(self, settings):
+class PerksManager:
+    def __init__(self, settings, player, weapon):
         self.settings = settings
-        self.perks = Perks(settings)
+        self.perks = Perks(settings, player, weapon)
         
         self.perks_dict = {"player_speed":self.perks.player_speed,
                            "player_attack_speed":self.perks.player_attack_speed,
                            "player_attack_damage":self.perks.player_attack_damage,
-                           "player_health":self.perks.player_health
+                           "player_max_health":self.perks.player_max_health,
+                           "player_size_up":self.perks.player_size_up,
+                           "player_size_down":self.perks.player_size_down,
+                           "player_regen":self.perks.player_regen,
+                           "projectil_speed":self.perks.projectile_speed
                            }
 
-    def choose_perks(self)->list:
+    def get_perks(self)->list:
         #utiliser des poids plus tard
         perks = []
         for _ in range(3):
-            perks.append(random.choice(self.perks_dict.keys()))
+            perks.append(random.choice(list(self.perks_dict.keys())))
         return perks
+
+    def choose_perk(self, perk):
+        self.perks_dict[perk]()
