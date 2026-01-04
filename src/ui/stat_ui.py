3d1215991eb66322bc478e528c9e558221ef8c19
Gabi <gabriel.leray03@gmail.com>
2026-01-04 17:49:12 +0100

    stat

--- src/ui/stat_ui.py
+++ src/ui/stat_ui.py
@@ -7,6 +7,9 @@ class StatUI:
         self.game_stats = game_stats
         # Rect des stats et surface qui accepte de modifier l'alpha
         self.stats_rect = pygame.Rect(50, 20, self.settings.screen_width//2-60, self.settings.screen_height-300)
+        self.bg_image1 = pygame.image.load(r"assets/images/cadre.png")
+        self.bg_image1 = pygame.transform.scale(self.bg_image1, (self.stats_rect.width, self.stats_rect.height))
+        
 
     def draw(self, screen, exit_rect, exit_text_rect, exit_text):
         """dessine l'interface complète 
@@ -18,11 +21,13 @@ class StatUI:
         screen.blit(bg_image, (0, 0))
 
         # Crée le cadre des stats
-        bg_image1 = pygame.image.load(r"assets/images/cadre.png")
-        bg_image1 = pygame.transform.scale(bg_image1, (self.stats_rect.width, self.stats_rect.height))
-        screen.blit(bg_image1, self.stats_rect)
-        self.stats_rect.center = (self.settings.screen_width//2, self.settings.screen_height//2)
-        self.stats_rect.y = 137
+        self.bg_image1.set_alpha()
+        self.bg_image1.get_rect(center = (self.settings.screen_width//2, self.settings.screen_height//2))
+        self.stats_rect.y = self.settings.screen_height//2 - self.stats_rect.height//2
+        self.stats_rect.x = self.settings.screen_width//2 - self.stats_rect.width//2
+        screen.blit(self.bg_image1, self.stats_rect)
+        
+        
 
         i = 0
         for key in self.game_stats.keys():
@@ -44,4 +49,8 @@ class StatUI:
         Recalcule les positions des éléments
         """
         self.stats_rect.update(50, 20, self.settings.screen_width//2 - 60, self.settings.screen_height -300)
+        self.bg_image1 = pygame.transform.scale(self.bg_image1, (self.stats_rect.width, self.stats_rect.height))
+
+        
+
         
